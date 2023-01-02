959a8d7329d8571b910e9c49d75472f6b706751e
balay <dlolhd@gmail.com>
2023-01-02 09:47:08 +0100

    Animating rotation with animateFloatAsState.

--- app/src/main/java/com/example/animatestate/MainActivity.kt
+++ app/src/main/java/com/example/animatestate/MainActivity.kt
@@ -3,13 +3,22 @@ package com.example.animatestate
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.animatestate.ui.theme.AnimateStateTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,10 +38,38 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-
+/**
+ * Animating rotation with animateFloatAsState
+ */
 @Composable
 fun RotationDemo() {
+    var rotated by remember { mutableStateOf(false) }
+    
+    val angle by animateFloatAsState(
+        targetValue = if (rotated) 360f else 0f,
+        //animationSpec = tween(durationMillis = 2500)
+        animationSpec = tween(durationMillis = 2500, easing = LinearEasing)
+    )
 
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.propeller),
+            contentDescription = "fan",
+            modifier = Modifier
+                .rotate(angle)
+                .padding(10.dp)
+                .size(300.dp)
+        )
+        Button(
+            onClick = { rotated = !rotated },
+            modifier = Modifier.padding(10.dp)
+        ) {
+            Text(text = "Rotate Propeller")
+        }
+    }
 }
 
 @Preview(showBackground = true)
@@ -42,3 +79,15 @@ fun DefaultPreview() {
         RotationDemo()
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
